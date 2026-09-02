9ba98760feb57b9bb6f17bb4f6c43768c27dc54c
Axel CHABAN <Axel.Chaban@ibm.com>
2026-09-02 15:37:49 +0200

    modif inqcust

--- src/base/cics/cobol/INQCUST.cbl
+++ src/base/cics/cobol/INQCUST.cbl
@@ -392,6 +392,7 @@
                  FUNCTION MOD(HV-CUSTOMER-CS-REVIEW-DATE / 100, 100)
               COMPUTE CUSTOMER-CS-REVIEW-DAY =
                  FUNCTION MOD(HV-CUSTOMER-CS-REVIEW-DATE, 100)
+ *********axel
               IF CUSTOMER-NUMBER = '000001234'
                  MOVE 'CHABAN' TO CUSTOMER-LAST-NAME OF CUSTOMER-NAME
               END-IF   
